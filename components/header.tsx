e3058ec90bdae8c84a9c1b70a7598120c855b56f
orkarfabianthewise <orkarmelch@gmail.com>
2025-05-21 23:38:40 +0100

    connection button brought to the front

--- components/header.tsx
+++ components/header.tsx
@@ -13,7 +13,7 @@ export default function Header({
   setShowResult,
 }: HeaderProps) {
   return (
-    <header className="flex justify-between items-center mb-8 sticky top-0">
+    <header className="flex justify-between items-center mb-8 sticky top-0 z-[50]">
       <div className="flex items-center gap-2">
         <Image src="/logo.png" width={50} height={50} alt="Logo" />
         <h1 className="hidden md:block text-xl font-bold bg-gradient-to-r from-gray-100 via-blue-100 to-gray-200 bg-clip-text text-transparent">
